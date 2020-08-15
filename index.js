da1abfed61885a9707b0a9974bbde24d6950339e
Aryan Jabbari <AryanJabbari@gmail.com>
2020-08-15 01:59:39 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+module.exports = {
+  globals: {
+    process: true,
+  },
+  env: { es6: true, jest: true, node: true },
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'prettier',
+    'prettier/@typescript-eslint',
+  ],
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'object-shorthand': 'error',
+    'import/order': [
+      'error',
+      {
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
+    'no-shadow': 'error',
+    '@typescript-eslint/camelcase': ['error', { properties: 'never' }],
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-use-before-define': 'off',
+  },
+};
---
+++ react.js
@@ -0,0 +1,21 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'prettier',
+    'prettier/@typescript-eslint',
+    './index',
+  ],
+  plugins: ['react-hooks'],
+  rules: {
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'error',
+  },
+  settings: { react: { version: 'detect' } },
+};
