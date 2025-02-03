213a2d3a2b25e6ec6b64d06b05228df7951f58b4
Aryan Jabbari <AryanJabbari@gmail.com>
2025-02-03 17:04:04 -0500

    Upgrade @typescript-eslint

--- react.js
+++ react.js
@@ -1,12 +1,12 @@
 module.exports = {
   env: { browser: true },
-  extends: ['plugin:react/recommended', './index'],
-  plugins: ['react-hooks'],
+  extends: ["plugin:react/recommended", "./index"],
+  plugins: ["react-hooks"],
   rules: {
-    'react/prop-types': 'off',
-    'react/react-in-jsx-scope': 'off',
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'error',
+    "react/prop-types": "off",
+    "react/react-in-jsx-scope": "off",
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "error",
   },
-  settings: { react: { version: 'detect' } },
+  settings: { react: { version: "detect" } },
 };
